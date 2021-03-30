95b486148be461ecfb851d689807f14832c62e3a
thang.hoviet <hovietthang1998@gmail.com>
2021-03-30 11:50:55 +0700

    up

--- src/main/java/com/controller/shop/Homepage.java
+++ src/main/java/com/controller/shop/Homepage.java
@@ -1,11 +1,14 @@
 package com.controller.shop;
 
+import com.model.Product;
 import com.repository.ProductRepository;
 import com.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Homepage {
@@ -14,7 +17,8 @@ public class Homepage {
 
     @GetMapping("/index")
     public String index(Model model){
-        model.addAttribute("findTop4ByOrderByIdDesc", service.findTop4ByOrderByIdDesc());
+        List<Product> top4ByOrderByIdDesc = service.findTop4ByOrderByIdDesc();
+        model.addAttribute("findTop4ByOrderByIdDesc", top4ByOrderByIdDesc);
         return "front-end/index";
     }
 }
