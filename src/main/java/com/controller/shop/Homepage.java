5e0225f07c1994695c799bd337b9111ad99728be
Domia <39hson@gmail.com>
2021-03-31 15:18:26 +0700

    test modelattribute

--- src/main/java/com/controller/shop/Homepage.java
+++ src/main/java/com/controller/shop/Homepage.java
@@ -1,5 +1,7 @@
 package com.controller.shop;
 
+import com.model.Category;
+import com.model.Product;
 import com.service.CategoryServiceImpl;
 
 import com.service.ProductServiceImpl;
@@ -9,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,20 +25,31 @@ public class Homepage {
     @Autowired
     CategoryServiceImpl categoryService;
 
+    @ModelAttribute("findTop4ByOrderByIdDesc")
+    public List<Product> findTop4ByOrderByIdDesc() {
+        return productService.findTop4ByOrderByIdDesc();
+    }
+
+    @ModelAttribute("findTop4ByOrderByRatingDesc")
+    public List<Product> findTop4ByOrderByRatingDesc() {
+        return productService.findTop4ByOrderByRatingDesc();
+    }
+
+    @ModelAttribute("findAllCategories")
+    public List<Category> findAllCategories() {
+        return categoryService.findALl();
+    }
+
 
     @GetMapping("/index")
-
-    public String index(@PageableDefault(size = 12) Pageable pageable, Model model){
+    public String index(@PageableDefault(size = 12) Pageable pageable, Model model) {
 //        Pageable pageable1 =
-        model.addAttribute("findTop4ByOrderByIdDesc", productService.findTop4ByOrderByIdDesc());
         model.addAttribute("selectAllPage12", productService.selectAll(pageable));
-        model.addAttribute("findTop4ByOrderByRatingDesc", productService.findTop4ByOrderByRatingDesc());
-        model.addAttribute("findAllCategories", categoryService.findALl());
 //        model.addAttribute("product", productService.findOne(id).get());
         return "front-end/index";
     }
 
-//<<<<<<< HEAD
+    //<<<<<<< HEAD
 //    @GetMapping("/product/quick-view/{id}")
 //    public String viewProduct(@PathVariable Long id, Model model){
 //        model.addAttribute("quickViewProduct", productService.findOne(id).get());
@@ -44,7 +58,7 @@ public class Homepage {
 //        return "back-end/product/product-view"; //sua duong dan
 //=======
     @GetMapping("/index/quick-view/{id}")
-    public String indexQuickView(@PathVariable Long id, Model model){
+    public String indexQuickView(@PathVariable Long id, Model model) {
         model.addAttribute("quickViewProduct", productService.findOne(id).get());
         return "front-end/shop-single-left-sidebar"; //sua duong dan
 
