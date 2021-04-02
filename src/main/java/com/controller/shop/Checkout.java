c6000ef318bda106731d72ad3ea458a6c3335a65
Domia <39hson@gmail.com>
2021-04-02 09:42:10 +0700

    done cart

--- src/main/java/com/controller/shop/Checkout.java
+++ src/main/java/com/controller/shop/Checkout.java
@@ -1,2 +1,12 @@
-package com.controller.shop;public class Checkout {
+package com.controller.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Checkout {
+    @GetMapping("/checkout")
+    public String showCheckout(){
+        return "front-end/checkout";
+    }
 }
