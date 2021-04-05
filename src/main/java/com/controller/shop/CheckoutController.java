bf1b365b6fd920676bd865b72be368c19006ae18
thang.hoviet <hovietthang1998@gmail.com>
2021-04-05 10:38:05 +0700

    d

--- src/main/java/com/controller/shop/CheckoutController.java
+++ src/main/java/com/controller/shop/CheckoutController.java
@@ -4,13 +4,11 @@ import com.model.Category;
 import com.model.Order;
 import com.model.OrderDetail;
 import com.model.Product;
-import com.repository.OrderRepository;
 import com.repository.ProvinceRepository;
 import com.service.CategoryService;
 import com.service.OrderDetailService;
 import com.service.OrderService;
 import com.service.ProductService;
-import com.service.impl.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -42,7 +40,7 @@ public class CheckoutController {
 
     @Autowired
     ProductService productService;
-    
+
     @ModelAttribute("findTop4ByOrderByIdDesc")
     public List<Product> findTop4ByOrderByIdDesc() {
         return productService.findTop4ByIsDeleteIsFalseOrderByIdDesc();
@@ -82,17 +80,13 @@ public class CheckoutController {
             model.addAttribute("searchMess", "Not found");
 
         Order orderSession = (Order) session.getAttribute("order");
-        orderService.saveCart(order, orderSession);
-//        order.setTotalPrice(orderSession.getTotalPrice());
-//        order.setOrderDetails(orderSession.getOrderDetails());
-//
-//        for (OrderDetail orderDetail1 : order.getOrderDetails()) {
-//            orderDetail1.setOrder(order);
-//        }
-//
-//        orderService.add(order);
-//        orderDetailService.saveAll(order.getOrderDetails());
-
+        order.setTotalPrice(orderSession.getTotalPrice());
+        order.setOrderDetails(orderSession.getOrderDetails());
+        orderService.add(order);
+        for (OrderDetail orderDetail1 : order.getOrderDetails()) {
+            orderDetail1.setOrder(order);
+            orderDetailService.add(orderDetail1);
+        }
         session.setAttribute("order", null);
         model.addAttribute("mess", "Order Success");
         return "front-end/checkout-result";
